d64d6bcbfdc921587922d3afbfdb6bcf101297cc
Abdullah AlAnsi <aalansi@yk-bank.com>
2024-10-13 03:08:44 +0300

    Bump version to 0.1.69 and add JavaScript logging for initialization and payment method handling

--- class-block.php
+++ class-block.php
@@ -1,25 +1,39 @@
 <?php
-require_once __DIR__.'/includes/BasgateHelper.php';
+require_once __DIR__ . '/includes/BasgateHelper.php';
+
 use Automattic\WooCommerce\Blocks\Payments\Integrations\AbstractPaymentMethodType;
 
-final class WC_Basgate_Blocks extends AbstractPaymentMethodType {
+final class WC_Basgate_Blocks extends AbstractPaymentMethodType
+{
 
-     private $gateway;
+    private $gateway;
     protected $name = 'basgate';
 
-    public function initialize() {
-        $this->settings = get_option( BasgateConstants::OPTION_DATA_NAME, [] );
-       // $this->gateway = new WC_Basgate(); 
-    } 
+    public function initialize()
+    {
+?>
+        <script>
+            console.log("===== STARTED initialize ");
+        </script>
+    <?php
+        $this->settings = get_option(BasgateConstants::OPTION_DATA_NAME, []);
+        // $this->gateway = new WC_Basgate(); 
+    }
 
     /*  public function is_active() {
         return $this->gateway->is_available();
     }   */
 
-    public function get_payment_method_script_handles() {
+    public function get_payment_method_script_handles()
+    {
+    ?>
+        <script>
+            console.log("===== STARTED get_payment_method_script_handles ");
+        </script>
+        <?php
         wp_register_script(
             'basgate-blocks-integration',
-            plugin_dir_url(__FILE__) .'assets/'.BasgateConstants::PLUGIN_VERSION_FOLDER.'/js/admin/checkout-block.js',
+            plugin_dir_url(__FILE__) . 'assets/' . BasgateConstants::PLUGIN_VERSION_FOLDER . '/js/admin/checkout-block.js',
             [
                 'wc-blocks-registry',
                 'wc-settings',
@@ -30,19 +44,23 @@ final class WC_Basgate_Blocks extends AbstractPaymentMethodType {
             null,
             true
         );
-        if( function_exists( 'wp_set_script_translations' ) ) {            
-            wp_set_script_translations( 'wc_basgate-blocks-integration');
-            
+        if (function_exists('wp_set_script_translations')) {
+            wp_set_script_translations('wc_basgate-blocks-integration');
         }
-        return [ 'basgate-blocks-integration' ];
+        return ['basgate-blocks-integration'];
     }
 
-    public function get_payment_method_data() {
+    public function get_payment_method_data()
+    {
+        ?>
+        <script>
+            console.log("===== STARTED get_payment_method_data ");
+        </script>
+<?php
         return [
-            'title' => __("Pay via Basgate",BasgateConstants::ID),
+            'title' => __("Pay via Basgate", BasgateConstants::ID),
             'description' => $this->settings['bas_description'],
         ];
-    } 
-
+    }
 }
 ?>
